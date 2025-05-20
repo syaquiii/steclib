35d8a6b6f3ba6c2493764aa2bd857668cd504940
syaquiii <msyauqioqi@gmail.com>
2025-05-20 18:01:41 +0700

    minor bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,9 +30,9 @@ class AuthController extends Controller
             $user = Auth::user();
 
             if ($user->is_admin) {
-                return redirect()->intended(route('page.home'));
+                return redirect()->intended(route('admin.dashboard'));
             } else {
-                return redirect()->intended(route('user.'));
+                return redirect()->intended(route('page.home'));
             }
         }
 
