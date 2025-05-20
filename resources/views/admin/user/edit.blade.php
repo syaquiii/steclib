ee351f95ee10fb7ae6438fc4ab018315ce4793fc
myaudn <mayaaudina040@gmail.com>
2025-05-21 04:57:05 +0700

    minor bug fix

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -15,7 +15,7 @@
 
             <div class="mb-4">
                 <label class="block text-[#1F305E] font-semibold mb-2">Username</label>
-                <input type="number" class="w-full px-4 py-2 border border-gray-300 rounded-lg bg-gray-200"
+                <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg bg-gray-200"
                     value="{{ $user->username }}" disabled>
             </div>
 
